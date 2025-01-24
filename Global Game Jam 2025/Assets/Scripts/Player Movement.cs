0c839a71633cc1c7481c5eb5de69b7e0adcd95eb
SiddPlus <sidd.ghosalkar@outlook.com>
2025-01-24 18:18:05 +0000

    Player Mouse Movement

--- Global Game Jam 2025/Assets/Scripts/Player Movement.cs
+++ Global Game Jam 2025/Assets/Scripts/Player Movement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] private int Power;
 
+    public float moveSpeed = 10f;  
+    public float screenBounds = 8f; 
+
+    private Vector3 mousePos;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,5 +25,14 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, Power);
         }
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 targetPosition = new Vector3(mousePos.x, transform.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        float clampedX = Mathf.Clamp(transform.position.x, -screenBounds, screenBounds);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
     }
 }
